efa95b1115915e897ea9e020db4a74a5fac816e2
Rick Wertenbroek <rick.wertenbroek@heig-vd.ch>
2020-02-19 14:29:30 +0100

    Added App (top level application)

--- canevas/src/main/scala/cnv/CanevasApp.scala
+++ canevas/src/main/scala/cnv/CanevasApp.scala
@@ -1,5 +1,147 @@
 package cnv
 
+import cnv.InputArgs._
+import cnv.Basic.pathExists
+
 object CanevasApp extends App {
   println("Canevas")
+
+  /////////////////////////
+  // Input args handling //
+  /////////////////////////
+  
+  val (task, options) = extractOptions(args) match {
+    case (MainOption("") , _) => printUsageAndQuit(); (MainOption(""), List())
+    case (_, List()) => quit(); (MainOption(""), List())
+    case opts => opts
+  }
+
+  task match {
+    case MainOption("signal") => generateSignals(options)
+    case MainOption("vcf") => generateVCFs(options)
+    case MainOption("regions") => taskRegions(options)
+    case _ => printUsageAndQuit()
+  }
+
+
+  ////////////////////
+  // Main functions //
+  ////////////////////
+
+  /** Generate VCFs */
+  def generateVCFs(options: List[Option]) = {
+    val (pairOptions, singleOptions) = getOptionsSplit(options)
+
+    val bamFile = pairOptions.find(_.option == "--input-bamfile") match {
+      case Some(value) if (pathExists(value.value)) => value.value
+      case Some(_) => println("Bam file not found !"); quit(); ""
+      case None => println("Missing bamfile !"); printUsageAndQuit(); ""
+    }
+
+    val signalDir = pairOptions.find(_.option == "--signal-dir") match {
+      case Some(value) if (pathExists(value.value)) => value.value
+      case Some(_) => println("Signal directory not found !"); quit(); ""
+      case None => println("Missing signal dir !"); printUsageAndQuit(); ""
+    }
+
+    val outputDir = pairOptions.find(_.option == "--output-dir") match {
+      case Some(value) if (pathExists(value.value)) => value.value
+      case Some(_) => println("Output directory not found !"); quit(); ""
+      case None => println("Missing output dir !"); printUsageAndQuit(); ""
+    }
+
+    val regions = pairOptions filter {_.option == "--region"} map {_.value}
+
+    regions match {
+        case List() => {
+          println("Start generating VCFs for whole genome...")
+          cnv.Canevas.allNiceVCFGeneration(bamFile, signalDir, outputDir)
+        }
+        case _ => {
+          // Per region
+          regions foreach {
+            println("Start generating VCFs for select regions...")
+            region => throw new Exception("TODO ! Not yet implemented")
+          }
+        }
+      }
+    println(s"Done generating VCFs, files are available in $outputDir")
+  }
+
+  /** Generate signals */
+  def generateSignals(options: List[Option]) = {
+    val (pairOptions, singleOptions) = getOptionsSplit(options)
+
+    val bamFile = pairOptions.find(_.option == "--input-bamfile") match {
+      case Some(value) if (pathExists(value.value)) => value.value
+      case Some(_) => println("Bam file not found !"); quit(); ""
+      case None => println("Missing bamfile !"); printUsageAndQuit(); ""
+    }
+
+    val outputDir = pairOptions.find(_.option == "--output-dir") match {
+      case Some(value) if (pathExists(value.value)) => value.value
+      case Some(_) => println("Output directory not found !"); quit(); ""
+      case None => println("Missing output dir !"); printUsageAndQuit(); ""
+    }
+
+    val regions = pairOptions filter {_.option == "--region"} map {_.value}
+
+    regions match {
+      case List() => {
+        println("Start extracting all signals for whole genome...")
+        cnv.Canevas.allSignalGeneration(bamFile, outputDir)
+      }
+      case _ => {
+        // Per region
+        regions foreach {
+          println("Start extracting all signals for select regions...")
+          region => cnv.Canevas.allSignalGenerationChr(bamFile, outputDir, region)
+        }
+      }
+    }
+    println(s"Done generating signals, files are available in $outputDir")
+  }
+
+  /** Prints the regions inside a bam file */
+  def taskRegions(options: List[Option]) = {
+    // Get the bamfile
+    val bamOptions = options filter {
+      _ match {
+        case OptionPair(option, value) => option == "--input-bamfile"
+        case _ => false
+      }
+    }
+
+    if (bamOptions.isEmpty) {
+      printUsageAndQuit()
+    } else {
+      bamOptions foreach {
+        _ match {
+          case OptionPair(_, value) => {
+            println(s"File : $value contains regions :")
+            try {
+              println("(" + cnv.SamToolBox.getRegions(value).map(_._1).toList.sorted.mkString(", ") + ")")
+            } catch {
+              case e: htsjdk.samtools.util.RuntimeIOException => println(s"File $value not found")
+            }
+          }
+          case _ => {}
+        }
+      }
+    }
+  }
+
+  //////////////////////
+  // Helper functions //
+  //////////////////////
+
+  def quit() = {
+    System.exit(1)
+  }
+
+  /** Prints the usage string and quits */
+  def printUsageAndQuit() = {
+    println(cnv.Strings.usage)
+    quit()
+  }
 }
---
+++ canevas/src/main/scala/cnv/InputArgs.scala
@@ -0,0 +1,84 @@
+package cnv
+
+import cnv.CanevasApp.printUsageAndQuit
+
+object InputArgs {
+
+  case class MainOption(option: String)
+
+  val mainOptions = Set(
+    MainOption("signal"),
+    MainOption("vcf"),
+    MainOption("regions")
+  )
+
+  case class OptionEntry(option: String, isPair: Boolean, docString: String, shortOption: String = "")
+
+  // These are the options accepted by the program
+  val options = Set(
+    OptionEntry("--help", false, "Prints help", "-h"),
+    OptionEntry("--input-bamfile", true, "Defines input bamfile", "-b"),
+    OptionEntry("--output-dir", true, "Defines the output directory", "-o"),
+    OptionEntry("--version", false, "Shows version", "-v"),
+    OptionEntry("--signal-dir", true, "Signal directory")
+  )
+
+  // Short to long option conversion map
+  val shortOptionsToLong = Map((options filter {_.shortOption != ""} map {o => (o.shortOption, o.option)}).toSeq: _*)
+  // The bimap of the above
+  val longOptionsToShort = Map(shortOptionsToLong.toList map {case (e1, e2) => (e2, e1)}: _*)
+
+  /** Changes the option to the original option version */
+  def normalizeOption(option: String) = {
+    shortOptionsToLong.getOrElse(option, option)
+  }
+
+  trait Option
+  case class OptionPair(option: String, value: String) extends Option
+  case class OptionSingle(option: String) extends Option
+  trait Documented {
+    def docString: String = ""
+  }
+
+  private val (sops, pops) = options partition {!_.isPair}
+
+  /** Set of all acceptable first position options */
+  val firstOptions = mainOptions map {_.option}
+  /** Set with all acceptable single option strings */
+  val singleOptions = (sops map {_.option}) ++ (sops map {opt => longOptionsToShort.getOrElse(opt.option, opt.option)})
+  /** Set with all acceptable pair option strings */
+  val pairOptions = (pops map {_.option}) ++ (pops map {opt => longOptionsToShort.getOrElse(opt.option, opt.option)})
+
+  def getOptionsSplit(optionList: List[Option]) = {
+    optionList.partition(_ match {
+      case OptionPair(_, _) => true
+      case OptionSingle(_) => false
+    }).asInstanceOf[(List[OptionPair], List[OptionSingle])]
+  }
+
+  /** Extracts options from the args array */
+  def extractOptions(args: Array[String]) = {
+    def nextOption(accOptionList: List[Option], list: List[String]): List[Option] = {
+      list match {
+        case Nil => {
+          accOptionList.reverse
+        }
+        case option :: tail if (singleOptions.contains(option)) => {
+          nextOption(OptionSingle(normalizeOption(option)) :: accOptionList, tail)
+        }
+        case option :: value :: tail if (pairOptions.contains(option)) => {
+          nextOption(OptionPair(normalizeOption(option), value) :: accOptionList, tail)
+        }
+        case _ => {
+          println("Unrecognized option !")
+          List()
+        }
+      }
+    }
+
+    args.toList match {
+      case head :: tail if (firstOptions.contains(head)) => (MainOption(head), nextOption(List(), tail))
+      case _ => (MainOption(""), List())
+    }
+  }
+}
